d2829d5e45f838abd1428af527ad3cd33f59eb0a
Jakub Karbowiak <jakub.karbowiak@gmail.com>
2016-02-19 23:49:43 +0100

    removed not needed @ComponentScan annotation

--- zosia-oasp/src/main/java/org/zosia/oasp/ZosiaOaspApplication.java
+++ zosia-oasp/src/main/java/org/zosia/oasp/ZosiaOaspApplication.java
@@ -2,9 +2,7 @@ package org.zosia.oasp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = "org.zosia.oasp")
 @SpringBootApplication
 public class ZosiaOaspApplication {
 
